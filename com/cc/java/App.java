ceb66d7c279b9e646bfc7c4a7e4f6bda6862bc39
SEA786 <shams.eltaf786@gmail.com>
2024-07-16 10:53:56 +0200

    04b/ getter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,68 +2,36 @@ package com.cc.java;
 
 public class App {
     
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
 
-        // Instanziierung  --> Erzeugen eines Objektes
-        // System.out.println(new Cat());
-
-        // cat --> Referenzvariable
-        // Cat --> Referenztyp(Klasse)
-        Cat cat = new Cat();
+        // Custom-Konstruktor
+        Cat cat = new Cat("Grizabella", "white", 29);
         output("Blick von aussen: " + cat);
         output("Blick von innen: " + cat.getInstanceVariable());
 
-
-        cat.name = "Grizabella";
-        cat.furcolor = "white";
-
-        cat.age = 29;
-
-
-        output(cat.name);
-        output(cat.furcolor);
-
-   
-        output (Integer.toString(cat.age));
+        // Werte ausgeben
+        output(cat.getName());
+        output(cat.getFurColor());
+        output(Integer.toString(cat.getAge())); 
 
         System.out.println("----------------------------");
 
-        Cat cat2 = new Cat();
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
         output("Blick von aussen: " + cat2);
         output("Blick von innen: " + cat2.getInstanceVariable());
-        
-   // Werte setzen
-   cat.name = "Grizabella";
-   cat.furColor = "white";
-   cat.age = 29;
 
-   // Werte ausgeben
-   output(cat.name);
-   output(cat.furColor);
-   output(Integer.toString(cat.age)); // Explizite Typumwandlung
+        // Werte ausgeben
+        output(cat2.getName());
+        output(cat2.getFurColor());
+        output(Integer.toString(cat2.getAge())); 
 
-
-
-   // Werte setzen
-   cat2.name = "Alonzo";
-   cat2.furColor = "grey";
-   cat2.age = 35;
-
-
-   // Werte ausgeben
-   output(cat2.name);
-   output(cat2.furColor);
-   output(Integer.toString(cat2.age)); // Explizite Typumwandlung
+        cat2.setAge(36);
+        output(Integer.toString(cat2.getAge())); 
 
     }
 
-      // Statische Methode, die in der Klasse ausgeführt wird ...
     public static void output(String outputStr){
         System.out.println(outputStr);
     }
    
 }
-
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,16 +1,54 @@
 package com.cc.java;
 
-public class Cat 
-{
-    public String name = "Grizabella";
-    public String furcolor = "white";
-    public int age ;
-    public String furColor;
+public class Cat {
+  
+    private String name; 
+    private String furColor;
+    private int age;  
+    
+    // Konstruktor (keinen Rückgabetyp)
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
 
+    
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
         return this;
-  
     }
+
+    /* Getter */
+    public String getName() {
+        return name;
+    }
+
+    public String getFurColor() {
+        return furColor;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    /* Setter */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFurColor(String furColor) {
+        this.furColor = furColor;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+
+    
+
+
 
 }
